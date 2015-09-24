13c106deb278f0502e004af90d8a4b02299945fb
thebudjr <tim.s.janney@gmail.com>
2015-09-24 01:32:00 -0400

    still trying to finagle to product/show layout

---
+++ app/overrides/product_left_part.rb
@@ -0,0 +1,4 @@
+Deface::Override.new(:virtual_path => "spree/products/show", 
+                     :name => "product_left_part",
+                     :set_attributes => '[data-hook="product_left_part"]',
+                     :attributes => {:class => 'col-md-8'})
---
+++ app/overrides/product_right_part.rb
@@ -0,0 +1,4 @@
+Deface::Override.new(:virtual_path => "spree/products/show", 
+                     :name => "product_right_part",
+                     :set_attributes => '[data-hook="product_right_part"]',
+                     :attributes => {:class => 'col-md-4'})
