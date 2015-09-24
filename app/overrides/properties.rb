8bfe3775b7864d37b2c6766b88e919b4199a65ab
thebudjr <tim.s.janney@gmail.com>
2015-09-24 00:03:41 -0400

    view improvements

---
+++ app/overrides/properties.rb
@@ -0,0 +1,3 @@
+Deface::Override.new(:virtual_path => "spree/products/_properties", 
+                     :name => "properties", 
+                     :remove => "h3")
---
+++ app/overrides/search_bar.rb
@@ -0,0 +1,3 @@
+Deface::Override.new(:virtual_path => "spree/shared/_nav_bar", 
+                     :name => "search_bar", 
+                     :remove => "li#search-bar")
