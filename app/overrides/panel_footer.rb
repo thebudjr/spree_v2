2b1c32a235b0920f096c1e84838b8dba0b29186e
thebudjr <tim.s.janney@gmail.com>
2015-09-24 00:20:40 -0400

    i need to start proof-reading

--- app/overrides/panel_footer.rb
+++ app/overrides/panel_footer.rb
@@ -1,3 +1,3 @@
 Deface::Override.new(:virtual_path => "spree/shared/_products", 
                      :name => "panel_footer", 
-                     :remove => ".panel-footer.")
+                     :remove => ".panel-footer")
