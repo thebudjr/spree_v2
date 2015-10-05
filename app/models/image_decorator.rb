03e30a253208ad93fc83bb2a2fb539eb9b916789
thebudjr <tim.s.janney@gmail.com>
2015-10-04 22:16:53 -0400

    added override.css to asset pipeline correctly...

--- app/models/image_decorator.rb
+++ app/models/image_decorator.rb
@@ -1,7 +1,7 @@
 Spree::Image.class_eval do
   attachment_definitions[:attachment][:styles] = {
     :mini => '80x100>', # thumbs under image
-    :small => '250x200>', # images on category view
+    :small => '200x250>', # images on category view
     :product => '1280x1600>', # full product image
     :large => '1600x2000>' # light box image
   }
