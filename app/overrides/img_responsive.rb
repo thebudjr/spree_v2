4d4f0a045bd75a9a9fef5a333cbe7a306896a680
thebudjr <tim.s.janney@gmail.com>
2015-10-04 21:42:25 -0400

    duh, i was targetting the div containing the img, not the img itself...

--- app/overrides/img_responsive.rb
+++
@@ -1,4 +0,0 @@
-Deface::Override.new(:virtual_path => "spree/shared/_products", 
-                     :name => "img_responsive", 
-                     :set_attributes => "div#product-body",
-                     :attributes => {:class => 'img-responsive'})
