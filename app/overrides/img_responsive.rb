2616285d053fd87f1fed048610fc5f1ac72bd324
thebudjr <tim.s.janney@gmail.com>
2015-10-04 20:39:55 -0400

    added img-responsive class to product page

---
+++ app/overrides/img_responsive.rb
@@ -0,0 +1,4 @@
+Deface::Override.new(:virtual_path => "spree/shared/_products", 
+                     :name => "img_responsive", 
+                     :set_attributes => "div#product-body",
+                     :attributes => {:class => 'img-responsive'})
