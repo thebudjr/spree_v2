9b0e4f2df623cdf65acfebd598a0739fdcd6d6ed
thebudjr <tim.s.janney@gmail.com>
2015-10-21 01:26:36 -0400

    split product page in half

--- app/overrides/product_left_part.rb
+++ app/overrides/product_left_part.rb
@@ -1,4 +1,4 @@
 Deface::Override.new(:virtual_path => "spree/products/show", 
                      :name => "product_left_part",
                      :set_attributes => '[data-hook="product_left_part"]',
-                     :attributes => {:class => 'col-md-8'})
+                     :attributes => {:class => 'col-md-6'})
--- app/overrides/product_right_part.rb
+++ app/overrides/product_right_part.rb
@@ -1,4 +1,4 @@
 Deface::Override.new(:virtual_path => "spree/products/show", 
                      :name => "product_right_part",
                      :set_attributes => '[data-hook="product_right_part"]',
-                     :attributes => {:class => 'col-md-4'})
+                     :attributes => {:class => 'col-md-6'})
