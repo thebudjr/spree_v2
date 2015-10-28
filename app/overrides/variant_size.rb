9b5870a4f57895904e363212941375f74fc44989
thebudjr <tim.s.janney@gmail.com>
2015-10-28 18:35:35 -0400

    changed variant to size

---
+++ app/overrides/variant_size.rb
@@ -0,0 +1,4 @@
+Deface::Override.new(:virtual_path => "spree/products/cart_form", 
+                     :name => "variant_size", 
+                     :replace => ".product-section-title",
+                     :text => "<h3>Available Sizes</h3>")
