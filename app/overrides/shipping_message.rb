c894ad754c1b733d4217102b558a2a64d666a6d5
thebudjr <tim.s.janney@gmail.com>
2015-10-28 18:22:31 -0400

    actually fixed shipping message this time

--- app/overrides/shipping_message.rb
+++ app/overrides/shipping_message.rb
@@ -1,4 +1,4 @@
 Deface::Override.new(:virtual_path => "spree/products/show", 
                      :name => "shipping_message", 
-                     :insert_after => ".cart-form",
+                     :insert_after => "#cart-form",
                      :text => "<div><p class='bg-info'>$15 flat-rate shipping on all orders.</p></div>")
