ebb2ac97218f95002601741794269d43f5f644a5
thebudjr <tim.s.janney@gmail.com>
2015-11-01 14:28:18 -0500

    correctly removed pictures from index page

--- app/overrides/remove_index.rb
+++ app/overrides/remove_index.rb
@@ -1,3 +1,3 @@
-Deface::Override.new(:virtual_path => "spree/products/index", 
+Deface::Override.new(:virtual_path => "spree/shared/_products", 
                      :name => "remove_index",
-                     :remove => '[data-hook="homepage_products"]')
+                     :remove => "#products")
