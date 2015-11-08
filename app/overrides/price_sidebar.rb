fff5622424282f3e3e48115911b026d92825d698
thebudjr <tim.s.janney@gmail.com>
2015-11-08 15:07:57 -0500

    restructured menu

--- app/overrides/price_sidebar.rb
+++ app/overrides/price_sidebar.rb
@@ -1,3 +1,3 @@
-Deface::Override.new(:virtual_path => "spree/shared/filters", 
+Deface::Override.new(:virtual_path => "spree/shared/_filters", 
                      :name => "price_sidebar", 
                      :remove => ".navigation")
---
+++ app/overrides/remove_taxonomy.rb
@@ -0,0 +1,3 @@
+Deface::Override.new(:virtual_path => "spree/shared/_taxonomies", 
+                     :name => "remove_taxonomy", 
+                     :remove => ".taxonomy-root")
